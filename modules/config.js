62ddc4d5a1501d956421fee04ac381a3e6f56bf5
Pafvel <91607056+Pafvel@users.noreply.github.com>
2023-01-09 23:51:19 +0100

    Added fonts

--- modules/config.js
+++ modules/config.js
@@ -5,3 +5,23 @@ DoD.gripTypes = {
     grip1h: "DoD.gripTypes.grip1h",
     grip2h: "DoD.gripTypes.grip2h"
 };
+
+DoD.supplyTypes = {
+    common: "DoD.supplyTypes.common",
+    uncommon: "DoD.supplyTypes.uncommon",
+    rare: "DoD.supplyTypes.rare",
+    unique: "DoD.supplyTypes.unique"
+};
+
+DoD.weaponFeatureTypes = {
+    bludgeoning: "DoD.weaponFeatureTypes.bludgeoning",
+    long: "DoD.weaponFeatureTypes.long",
+    mounted: "DoD.weaponFeatureTypes.mounted",
+    noparry: "DoD.weaponFeatureTypes.noparry",
+    piercing: "DoD.weaponFeatureTypes.piercing",
+    slashing: "DoD.weaponFeatureTypes.slashing",
+    subtle: "DoD.weaponFeatureTypes.subtle",
+    thrown: "DoD.weaponFeatureTypes.thrown",
+    toppling: "DoD.weaponFeatureTypes.toppling",
+    shield: "DoD.weaponFeatureTypes.shield"
+};
