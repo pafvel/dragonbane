4f4c48fc60e8c6792672fdc72ea0d1b87f575452
Pafvel <91607056+Pafvel@users.noreply.github.com>
2023-01-17 08:56:24 +0100

    Spell tab

--- modules/actor.js
+++ modules/actor.js
@@ -1,4 +1,5 @@
 import DoDE_Utility from "./utility.js";
+import { DoD } from "./config.js";
 
 export class DoDActor extends Actor {
 
@@ -55,6 +56,7 @@ export class DoDActor extends Actor {
     _prepareCharacterData() {
         if (this.type !== 'character') return;
         this._prepareBaseChances();
+        this._prepareSpellValues();
     }
 
     _prepareNpcData() {
@@ -78,8 +80,49 @@ export class DoDActor extends Actor {
                 let name = skill.system.attribute;
                 let value = this._getAttributeValueFromName(name);
                 skill.baseChance = DoDE_Utility.calculateBaseChance(value);
-                if (skill.system.skillType != "secondary" && skill.system.value < skill.baseChance) {
+                if ((skill.system.skillType == "core" || skill.system.skillType == "weapon") && skill.system.value < skill.baseChance) {
                     skill.system.value = skill.baseChance;
+                }
+            }
+        }
+    }
+
+    _prepareSpellValues() {
+        let magicSchools = new Map;
+        let maxValue = 0;
+
+        // find skill values for schools of magic
+        for (let item of this.items.contents) {
+            if (item.type == "skill" && item.system.skillType == "magic") {
+                let skill = item;
+                magicSchools.set(skill.name, skill.system.value);
+                if (skill.system.value > maxValue) {
+                    maxValue = skill.system.value;
+                }
+            }
+        }
+
+        // set the skill value of general spells to max of all magic schools
+        let generalSchool = "DoD.spell.general";
+        magicSchools.set(generalSchool, maxValue);
+
+        let generalSchoolLocalized = game.i18n.localize(generalSchool);
+
+        for (let item of this.items.contents) {
+            if (item.type == "spell") {
+                let spell = item;
+
+                // replace general spells school name with localized string if it matches
+                if (spell.system.school == generalSchoolLocalized) {
+                    spell.system.school = generalSchool;
+                    spell.update({ ["system.school"]: generalSchool});
+                }
+
+                // set skill values for spell corresponding to school
+                if (magicSchools.has(spell.system.school)) {
+                    spell.value = magicSchools.get(spell.system.school);
+                } else {
+                    spell.value = 0;
                 }
             }
         }
