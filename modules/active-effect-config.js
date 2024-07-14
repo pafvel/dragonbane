5f189d0f0821a64c7f29ed48518be5285ce825fc
Pafvel <91607056+Pafvel@users.noreply.github.com>
2024-07-14 14:57:47 +0200

    style active effect sheet

--- modules/active-effect-config.js
+++ modules/active-effect-config.js
@@ -1,6 +1,15 @@
 import { DoD } from "./config.js";
 
 export default class DoDActiveEffectConfig extends ActiveEffectConfig {
+
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions,  {
+            classes: ["DoD", "sheet", "active-effect-sheet"],
+            width: 600,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+        });
+    }
+
     get template() {
         return "systems/dragonbane/templates/active-effect-config.html";
     }
