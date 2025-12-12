ae1b06608eb0d336efe23a9c88650b54e3abd56f
Pafvel <91607056+Pafvel@users.noreply.github.com>
2025-12-12 15:23:04 +0100

    Token ruler grid highlight style

--- modules/token-ruler.js
+++ modules/token-ruler.js
@@ -4,6 +4,13 @@ export default class DoDTokenRuler extends BaseRuler {
     
     static WAYPOINT_LABEL_TEMPLATE = "systems/dragonbane/templates/partials/waypoint-label.hbs";
 
+    static GRID_HIGHLIGHT_STYLES = {
+        move: {color: 0x00604D},
+        dash: {color: 0xaaaa00},
+        outOfRange: {color: 0xE83031}
+    };
+
+    /** @override */
     _getWaypointLabelContext(waypoint, state) {
         const context = super._getWaypointLabelContext(waypoint, state);
         if (context?.cost?.units === "m") {
@@ -18,5 +25,21 @@ export default class DoDTokenRuler extends BaseRuler {
             }
         }
         return context;
+    }
+
+    /** @override */
+    _getGridHighlightStyle(waypoint, offset) {
+        if (!this.token.actor || waypoint.action === "blink") {
+            return super._getGridHighlightStyle(waypoint, offset);
+        }
+        const movement = this.token.actor.system.movement.value;
+        const cost = waypoint.measurement.cost;
+        if (cost > 2 * movement) {
+            return DoDTokenRuler.GRID_HIGHLIGHT_STYLES.outOfRange;
+        } else if (cost <= movement) {
+            return DoDTokenRuler.GRID_HIGHLIGHT_STYLES.move;
+        } else {
+            return DoDTokenRuler.GRID_HIGHLIGHT_STYLES.dash;
+        }
     }
 }
