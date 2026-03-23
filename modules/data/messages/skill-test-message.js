9bede6c72cc466997ceb1736a3565e086fa3919f
Pafvel <91607056+Pafvel@users.noreply.github.com>
2026-03-23 16:39:44 +0100

    fix: monster defend chat message missing skill reference

--- modules/data/messages/skill-test-message.js
+++ modules/data/messages/skill-test-message.js
@@ -8,22 +8,31 @@ export default class DoDSkillTestMessageData extends DoDTestMessageBaseData {
         const { fields } = foundry.data;
         return this.mergeSchema(super.defineSchema(), {
             skillUuid: new fields.StringField({ required: true, initial: "" }),
+            skillName: new fields.StringField({ required: true, initial: "" }),
+            skillValue: new fields.NumberField({ required: true, initial: 0 }),
         });
     }
 
     toContext() {
         const context = super.toContext();
 
-        // replace skillUuid with skill
-        context.skill = fromUuidSync(this.skillUuid);
+        // replace skillUuid with skill, or create a pseudo skill object for monster defend rolls
+        if (this.skillUuid) {
+            context.skill = fromUuidSync(this.skillUuid);
+        } else {
+            context.skill = { name: this.skillName, system: { value: this.skillValue } };
+        }
         delete context.skillUuid;
+        delete context.skillName;
+        delete context.skillValue;
+        
         return context;
     }
 
     static prepareSource(context) {
-         // replace skill with skillUuid
+         // replace skill with skillUuid or (for monster defend rolls) skillName and skillValue
         const { skill, ...rest } = super.prepareSource(context);
-        return { ...rest, skillUuid: skill?.uuid };
+        return { ...rest, skillUuid: skill?.uuid, skillName: skill?.name, skillValue: skill?.system.value };
     }
 
     static fromContext(context) {
@@ -33,10 +42,10 @@ export default class DoDSkillTestMessageData extends DoDTestMessageBaseData {
 
     formatRollMessage() {
         const result = this.formatRollResult();
-        const content = game.i18n.format("DoD.roll.skillRollUUID", {
-            uuid: this.skillUuid,
-            result: result
-        });
+        const content = this.skillUuid
+        ? game.i18n.format("DoD.roll.skillRollUUID", { uuid: this.skillUuid, result: result})
+        : game.i18n.format("DoD.roll.skillRoll", { skill: this.skillName, result: result });
+
         return {
             user: game.user.id,
             speaker: ChatMessage.getSpeaker({ actor: this.actor }),
