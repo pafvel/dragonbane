7c7fe603504e78add4ac92b63b17851eb7b0da0c
BartlomiejBielecki <bartlomiejbielecki@gmail.com>
2026-04-23 12:45:41 +0200

    automatic ability change for NPC with Robust/Focused

--- modules/data/actors/NPCData.js
+++ modules/data/actors/NPCData.js
@@ -11,4 +11,24 @@ export default class DoDNPCData extends DoDCharacterBaseData {
     static migrateData(source) {
         return super.migrateData(source);
     }
+    prepareDerivedData() {
+        super.prepareDerivedData();
+        this.checkHA();
+      }
+
+        checkHA() {
+            const abilitys = this.parent.items.filter(i => i.type === "ability");
+            abilitys.forEach(ability => {
+                if(ability.system.secondaryAttribute === "hitPoints") {
+                    this.hitPoints.max += 2;
+                    this.hitPoints.value += 2;
+                    this.hitPoints.base += 2;
+                }
+                if(ability.system.secondaryAttribute === "willPoints") {
+                    this.willPoints.max += 2;
+                    this.willPoints.value += 2;
+                    this.willPoints.base += 2;
+                }
+            });
+        }
 }
