a20b1c38fa7e185154381c723da6b2e06b4625da
Pafvel <91607056+Pafvel@users.noreply.github.com>
2026-06-21 14:18:40 +0200

    fix: incorrect speaker in some cases

--- modules/data/messages/skill-test-message.js
+++ modules/data/messages/skill-test-message.js
@@ -48,7 +48,7 @@ export default class DoDSkillTestMessageData extends DoDTestMessageBaseData {
 
         return {
             user: game.user.id,
-            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+            speaker: ChatMessage.getSpeaker({ actor: fromUuidSync(this.actorUuid) }),
             content: "<p>" + content + "</p>"
         };
     }
